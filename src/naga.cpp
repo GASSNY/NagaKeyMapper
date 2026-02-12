d527566669ddc6032003e7858dcc530d4ff5ceb6
Henri W. Sirkia <henri@sirkia.es>
2026-02-13 00:51:42 +0100

    Gnome Wayland, ydotool

--- src/naga.cpp
+++ src/naga.cpp
@@ -1,16 +1,3 @@
-/* See https://github.com/RaulPPelaez/Naga_KeypadMapper/graphs/contributors
-* for a full list of contributors.
-* This program is free software. It comes without any warranty, to the extent
-* permitted by applicable law. You can redistribute it and/or modify it under the
-* terms of the Beer-ware license revision 42.
-* ----------------------------------------------------------------------------
-* "THE BEER-WARE LICENSE" (Revision 42):
-* RaulPPelaez, et. al wrote this file.  As long as you retain this notice you
-* can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a beer in return. RaulPPelaez 2016
-* ----------------------------------------------------------------------------
-*/
-
 #include <cstdio>
 #include <iostream>
 #include <string>
